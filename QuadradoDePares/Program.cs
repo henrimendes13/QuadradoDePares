ecbde4b98122f1bd4dd927a1df14c1f62f8b044f
Henrique Mendes <henriquemendes13@gmail.com>
2023-09-24 21:46:21 -0300

    Add project files.

---
+++ QuadradoDePares/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Leia um valor inteiro N. Apresente o quadrado de cada um dos valores pares, 
+de 1 até N, inclusive N, se for o caso.*/
+
+int n;
+n = int.Parse(Console.ReadLine());
+
+for (int i=2; i <=n; i=i+2)
+{
+    Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+
+}
